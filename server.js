e596f755aa7bb5c4d53671c28ef61feb8f71fef4
Anish Patil <apatil@redhat.com>
2015-04-09 10:06:26 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,146 @@
+#!/bin/env node
+//  OpenShift sample Node application
+//var express = require('express');
+//var fs      = require('fs');
+var irc = require('irc');
+
+var bot = new irc.Client('irc.devel.redhat.com', process.env.OPENSHIFT_APP_NAME || 'ircbot', {
+    channels: ['#eng-i18n','#test'],
+    port: 8001,
+    debug: true
+});
+
+/*
+var SampleApp = function() {
+
+
+    self.setupVariables = function() {
+        //  Set the environment variables we need.
+        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+        if (typeof self.ipaddress === "undefined") {
+            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+            //  allows us to run/test the app locally.
+            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+            self.ipaddress = "127.0.0.1";
+        };
+    };
+
+
+
+    self.populateCache = function() {
+        if (typeof self.zcache === "undefined") {
+            self.zcache = { 'index.html': '' };
+        }
+
+        //  Local cache for static content.
+        self.zcache['index.html'] = fs.readFileSync('./index.html');
+    };
+
+
+
+    self.cache_get = function(key) { return self.zcache[key]; };
+
+
+
+    self.terminator = function(sig){
+        if (typeof sig === "string") {
+           console.log('%s: Received %s - terminating sample app ...',
+                       Date(Date.now()), sig);
+           process.exit(1);
+        }
+        console.log('%s: Node server stopped.', Date(Date.now()) );
+    };
+
+
+
+    self.setupTerminationHandlers = function(){
+        //  Process on exit and signals.
+        process.on('exit', function() { self.terminator(); });
+
+        // Removed 'SIGPIPE' from the list - bugz 852598.
+        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
+         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
+        ].forEach(function(element, index, array) {
+            process.on(element, function() { self.terminator(element); });
+        });
+    };
+
+
+    self.createRoutes = function() {
+        self.routes = { };
+
+        self.routes['/asciimo'] = function(req, res) {
+            var link = "http://i.imgur.com/kmbjB.png";
+            res.send("<html><body><img src='" + link + "'></body></html>");
+        };
+
+        self.routes['/'] = function(req, res) {
+            res.setHeader('Content-Type', 'text/html');
+            res.send(self.cache_get('index.html') );
+        };
+    };
+
+
+
+    self.initializeServer = function() {
+        self.createRoutes();
+        self.app = express.createServer();
+
+        //  Add handlers for the app (from the routes).
+        for (var r in self.routes) {
+            self.app.get(r, self.routes[r]);
+        }
+    };
+
+
+
+    self.initialize = function() {
+        self.setupVariables();
+        self.populateCache();
+        self.setupTerminationHandlers();
+
+        // Create the express server and routes.
+        self.initializeServer();
+    };
+
+
+
+    self.start = function() {
+        //  Start the app on the specific interface (and port).
+        self.app.listen(self.port, self.ipaddress, function() {
+            console.log('%s: Node server started on %s:%d ...',
+                        Date(Date.now() ), self.ipaddress, self.port);
+        });
+    };
+
+};  
+
+
+
+
+var zapp = new SampleApp();
+zapp.initialize();
+zapp.start();
+*/
+
+
+bot.addListener('message', function(from, to, message) {
+    if(  message.indexOf('who is there?') > -1
+      || message.indexOf("who's there?") > -1
+      || message.indexOf("Who's there?") > -1
+      || message.indexOf("Who is there?") > -1
+      )
+    {
+        bot.say(to, 'Doris');
+    }
+});
+bot.addListener('message', function(from, to, message) {
+    if(  message.indexOf('Doris who?') > -1
+      || message.indexOf("doris who?") > -1
+     )
+    {
+        bot.say(to, "Doris locked, that's why I'm knocking!");
+    }
+});
